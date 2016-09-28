04da87347a365704ed5c1ea431315183c150aef0
Cipher032 <solowingpixycipher@gmail.com>
2016-09-27 22:15:08 -0700

    Update 62KmkI.c

--- 62KmkI.c
+++ 62KmkI.c
@@ -1,17 +1,23 @@
 //wallbot code Mk I
 task autonomous(){
+	startup();
+	//insert autonomous code here
 	AutonomousPlaceholderForTesting();
 }
 
 task usercontrol()
 {
+	startup();
 	while(true){
+		//insert remote control code here
 		UserControlPlaceholderForTesting();
 	}
-
-
 }
 
 void pre_auton(){
+	
+}
+
+void startup(){
 	
 }
