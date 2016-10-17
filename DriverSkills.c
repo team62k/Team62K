850c96f4ca7528ef89d16179d99276eb689c5093
Abner B <jorgeabner2000@gmail.com>
2016-10-17 16:37:16 -0700

    Methods for DriverSkills.c

--- DriverSkills.c
+++ DriverSkills.c
@@ -35,6 +35,75 @@ long            motor_driveL;            /// LEFT
 ///// Since: Oct. 16, 2016                              /////
 *////////////////////////////////////////////////////////////
 
+//setters
+void
+setMotorL( int valueL )
+{
+  motor[ LDriveBase ] = valueL;
+}
+
+void
+setMotorR( int valueR )
+{
+  motor[ RDriveBase ] = valueR;
+}
+
+void
+setArms( int power )
+{
+  motor[ leftLift ] = power;
+  motor[ rightLift ] = power;
+}
+
+void
+setArms( int power )
+{
+  motor[ leftLift ] = power;
+  motor[ rightLift ] = power;
+}
+
+void
+setGrabber( int power )
+{
+  motor[ grabber ] = power;
+}
+
+
+//action methods
+void
+liftTester(int power)
+{
+  setArms(power);
+  delay(150);
+  setArms(power);
+  delay(300);
+  setArms(-power);
+  delay(100);
+}
+
+void
+move( int time, int power )
+{
+  setMotorL(power);
+  setMotorR(power);
+  delay(time);
+  power = 0; //what happens when i ommit this portion?
+  setMotorR(power);
+  setMotorL(power);
+  //no delay( )?
+
+}
+
+//stops everything
+void
+stopAll()
+{
+  move(1000, 0);
+  setArms(0);
+  setGrabber(0);
+  //add any other motors that need killing 🗡
+}
+
 
 // You may want to perform some actions before the competition starts. Do them in the
 // following function.
@@ -60,35 +129,39 @@ task autonomous()
 // This task is used to control your robot during the user control phase of a VEX Competition.
 // You must modify the code to add your own robot specific commands here.
 
+#warning "usercontrol task"
+//try making this a tankDrive()
 task usercontrol()
 {
-	//start PID control task here "startTask( )"
+  //start PID control task here "startTask( )"
   while (true) //try using '1'
-{
+  {
+    //left drive with deadbands
+    setMotorL(abs(vexRT[ Ch3 ]) > 15 ? vexRT[ Ch3 ] : 0);
 
-		//left drive with deadbands
-		motor[ FLWheel ] = abs(vexRT[ Ch3 ]) > 15 ? vexRT[ Ch3 ] : 0;
-		motor[ BLWheel ] = abs(vexRT[ Ch3 ]) > 15 ? vexRT[ Ch3 ] : 0;
-
-		//right drive with deadbands
-		motor[ FRWheel ] = abs(vexRT[ Ch2 ]) > 15 ? vexRT[ Ch2 ] : 0;
-		motor[ BRWheel ] = abs(vexRT[ Ch2 ]) > 15 ? vexRT[ Ch2 ] : 0;
-
-		delay(100); //give it some time
-
-		//operate the lift
-		if(vexRT[ Btn5U ] || vexRT[ Btn5U ]) //test with == 1
-		{
-			motor[ lift ] = (vexRT[ Btn5U ] - vexRT[ Btn5U ]) * -127;
-		}
-		else motor[ lift ] = 0; //is this necessary?
+    //right drive with deadbands
+    setMotorR(abs(vexRT[ Ch2 ]) > 15 ? vexRT[ Ch2 ] : 0);
 
 
-		//killswitch
-		if(vexRT[ Btn8D ] == 1)
-		{
-			stopAll();
-		}
-		wait1Msec(20); //don't hog the CPU :)
+    //operate the lift
+    if(vexRT[ Btn5U ] || vexRT[ Btn6U ]) //test with == 1
+    {
+      setArms((vexRT[ Btn6U ] - vexRT[ Btn5U ]) * -127);
+    }
+    else setArms(0); //is this necessary?
+
+
+    //killswitch
+    if(vexRT[ Btn8D ] == 1)
+    {
+      stopAll();
+    }
+    wait1Msec(20); //don't hog the CPU :)
   }
+
+  if(vexRT[ Btn5D ] || vexRT[ Btn6D ])
+  {
+    setGrabber((vexRT[ Btn6U ] - vexRT[ Btn5U ]) * -127);
+  }
+  else setGrabber(0); //is this necessary?
 }
